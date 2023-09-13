30617311cc61f1e972849fa8e7ca1850bb947793
max <mzhang@moblabs.io>
2023-09-12 18:08:57 -0700

    added dotfiles

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -137,10 +137,10 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
